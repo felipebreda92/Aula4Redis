a0e0f69fb577d5b192caf680439ffe5f9846a147
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-07 21:29:47 -0200

    Inicial Commit

---
+++ Aula4Redis/Aula4Redis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Aula4Redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connect
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+
+            Console.WriteLine(db.StringSet("A", "1"));
+            Console.WriteLine(db.StringGet("A"));            
+            Console.WriteLine(db.StringIncrement("A"));
+            Console.WriteLine(db.StringGet("A"));
+            Console.WriteLine((db.SetAdd("TECH", "SQL")));
+            Console.WriteLine((db.HashSet("CLI", "AA", "1")));
+            Console.WriteLine(db.ListLeftPush("L1", "A"));
+            Console.WriteLine(db.ListLeftPush("L1", "B"));
+            Console.ReadKey();
+            
+        }
+    }
+}
