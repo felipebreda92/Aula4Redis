9e2648eb6018bffad73f1ccbfa5a61c31926a992
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-07 22:48:07 -0200

    Chupa Breda

---
+++ Aula4Redis/PubRedis/Program.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace PubRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Connect
+            var redis = ConnectionMultiplexer.Connect("40.77.30.246");
+            IDatabase db = redis.GetDatabase();
+            var sub = redis.GetSubscriber();
+            sub.Publish("FIAP14", "Teste Publish");
+        }
+    }
+}
---
+++ Aula4Redis/SubRedis/Program.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Text.RegularExpressions;
+
+namespace SubRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var redis = ConnectionMultiplexer.Connect("40.77.30.246");
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+                        
+            var sub = redis.GetSubscriber();
+
+            sub.Subscribe("PERGUNTAS", (ch, msg) =>
+            {
+                db.HashSet(ObterIdPergunta(msg), "EQUIPE2", ObterPergunta(msg));
+            });
+            Console.ReadKey();
+        }
+
+        public static string ObterPergunta(string msg)
+        {
+            Regex obterPergunta = new Regex(@"(?<=:\s*).*\s*$");
+            Match resultado = obterPergunta.Match(msg);
+            return resultado.Value;
+        }
+
+        public static string ObterIdPergunta(string msg)
+        {
+            Regex obterIdPergunta = new Regex(@".*(?=:)");
+            Match resultado = obterIdPergunta.Match(msg);
+            return resultado.Value;            
+        }
+    }
+
+}
